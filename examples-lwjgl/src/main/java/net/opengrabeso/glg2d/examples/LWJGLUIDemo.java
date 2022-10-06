b292d0a4be77a77b7107bf505adced2d60058cc1
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2022-10-06 15:37:34 +0200

    Rename demo class

--- examples-lwjgl/src/main/java/net/opengrabeso/glg2d/examples/LWJGLUIDemo.java
+++ examples-lwjgl/src/main/java/net/opengrabeso/glg2d/examples/LWJGLUIDemo.java
@@ -1,5 +1,5 @@
 package net.opengrabeso.glg2d.examples;
 
-public class LWJGLlUIDemo {
+public class LWJGLUIDemo {
     public static void main(String[] args) {LWJGLExampleFactory.display(args, new UIDemo());}
 }
